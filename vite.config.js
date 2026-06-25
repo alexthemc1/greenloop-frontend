d7a77051a1b88ae340f96f944406abd5baf148b8
Alexandre <Alexandre4si17@gmail.com>
2026-06-25 17:50:04 +0200

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/greenloop/',
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
